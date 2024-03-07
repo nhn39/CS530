2dff28977b5eb3d20a9ea153aecf461a505291b2
hoangnamtbp <123501097+hoangnamtbp@users.noreply.github.com>
2024-03-07 05:33:25 -0500

    server _side

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import { userRoute } from './routes/userRoute.js';
+import { residencyRoute } from './routes/residencyRoute.js';
 dotenv.config()
 
 const app = express();
@@ -16,3 +18,6 @@ app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
 
 });
+
+app.use('/api/user', userRoute)
+app.use("/api/residency", residencyRoute)
